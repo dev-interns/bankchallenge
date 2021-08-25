e49a859f70e00e3577e915aad63e7ba86b965abe
Juan Felipe Marin Florez <jumarin@javeriana.edu.co>
2021-08-25 12:44:48 -0500

    created AuditService

---
+++ src/com/endava/bankchallenge/observer/AuditService.java
@@ -0,0 +1,10 @@
+package com.endava.bankchallenge.observer;
+
+import com.endava.bankchallenge.model.MessageTransaction;
+
+public class AuditService implements IObserverTransaction{
+    @Override
+    public void Update(MessageTransaction mensaje) {
+
+    }
+}
